c1f2314623aad9e52f9602c11678d5c721e54526
Eugene <elopukhov@gmail.com>
2015-07-18 03:43:23 +0000

    allowed hosts added

--- sb_api/sb_api/settings.py
+++ sb_api/sb_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3ky#si9ill*ta3qekf+r3qvhox&l9ly7cqv@y!1js1%hz=z+8z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '192.241.165.96']
 DEFAULT_PORT = '8080'
 
 # Application definition
